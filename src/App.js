ca6e4913eeb80dc20c86e299b34087f8613923a5
shakilmahammud <hshakil987@gmail.com>
2021-03-30 14:04:45 +0600

    multer file upload sysem handle front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import axios from 'axios'
 
 function App() {
+  const [files,setFiles]=useState()
+  const [message,setmessage]=useState()
+  const onSubmit = async e =>{
+    e.preventDefault();
+    const formData=new FormData()
+    formData.append("message",message)
+    for(let index=0; index<files.length; index++){
+      const avatar =files[index]
+      formData.append("files",avatar)
+    }
+  const result = await axios.post('http://localhost:30001/multifiles',formData)
+    console.log(result)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit}>
+      <input type="file" name="avatar"  multiple onChange={e=>{
+        const filelist=e.target.files
+        setFiles(filelist)
+      }}/>
+      <input type="text" name="message"  onChange={e=>setmessage(e.target.value)}/>
+      <input type="submit" />
+    </form>
   );
 }
 
